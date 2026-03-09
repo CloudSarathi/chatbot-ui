f91c6b7326e4026801b6f734df764db97cfa864d
𝙰𝙽𝙸𝙻𝙺𝚄𝙼𝙰𝚁 <awstraining061@gmail.com>
2026-03-09 20:22:38 +0530

    Update output.tf

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.JioHotstar.id
+  value = aws_eks_cluster.openai.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.JioHotstar.id
+  value = aws_eks_node_group.openai.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.Jio_vpc.id
+  value = aws_vpc.openai_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.Jio_subnet[*].id
+  value = aws_subnet.openai_subnet[*].id
 }
